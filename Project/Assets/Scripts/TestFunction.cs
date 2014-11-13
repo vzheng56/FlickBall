ed1c4458e7a48d77069e205a5589731c6bd1ab6c
渔夫 <vzheng56@gmail.com>
2014-11-13 19:37:41 +0800

    Add Project
    主要的功能目录

---
+++ Project/Assets/Scripts/TestFunction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFunction : MonoBehaviour
+{
+        void Start() {
+
+        GameObject Socker = Instantiate(Resources.Load("Socker", typeof(GameObject))) as GameObject;
+        Socker.name = "CurrentSocker";
+        Socker.transform.position = Vector3.zero;
+    }
+}
